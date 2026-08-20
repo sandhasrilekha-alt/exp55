249823013ba8d3f727eab3a38dfae3474e0ffa03
sandhasrilekha-alt <sandhasrilekha@gmail.com>
2026-08-20 15:32:05 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize two variables
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Calculate the sum using the + operator
+        int sum = num1 + num2;
+        
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
